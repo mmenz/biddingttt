09ac3888d940aeaf2d7753a88898a3e995e8066f
Michael Menz <michael.menz.developer@gmail.com>
2014-08-26 19:29:12 -0400

    Make Play Well
    Made the computer play optimally

--- biddingttt.py
+++ biddingttt.py
@@ -3,7 +3,6 @@ import os
 import random
 import ttt
 
-
 class index(object):
 	@cherrypy.expose
 	def index(self):
@@ -20,31 +19,44 @@ class computerBidder(object):
 		p1 = int(args['p1'])
 		p2 = int(args['p2'])
 		pos = list(args['pos'])
-		if ttt.evenGameQ(pos):
-			if p1 > p2:
-				response = p1
-			else:
-				response = random.randint(0, int(p1/2))
-		elif ttt.tieGameQ(pos):
+		if p2 == 0:
+			response = 0
+		elif ttt.squaresRemainingQ(pos) == 1: 	# last square = bet it all
+			response = p1
+		elif ttt.evenGameQ(pos): 				# next move wins = bet it all
+			response = p1
+		elif ttt.tieGameQ(pos): 				# game will end tie = random bids
 			response = random.randint(0, p1)
-		else:
-			response = random.randint(0, p1)
+		else:									# OTHER CASES = TO DO
+			strat = ttt.getStrategy(pos, p1, p2)
+			r = random.random()
+			count = 0
+			while r > 0:
+				r -= strat[count]
+				count += 1
+			response = count-1
 		return str(response)
 
 class computerMover(object):
 	exposed = True
-	
+
 	@cherrypy.tools.accept(media='text/plain')
 	def POST(self, **args):
 		p1 = int(args['p1'])
 		p2 = int(args['p2'])
 		pos = list(args['pos'])
-		if ttt.winningSquareQ(pos) >= 0:
+		if p2 == 0:
+			response = randint(0,8)
+			while pos[response] != 'n':
+				response = randint(0,8)
+		elif ttt.tieGameQ(pos): 
+			response = randint(0,8)
+			while pos[response] != 'n':
+				response = randint(0,8)
+		elif ttt.winningSquareQ(pos) >= 0:
 			response = ttt.winningSquareQ(pos)
 		else:
-			response = random.randint(0, 8)
-			while pos[response] != 'n':
-				response = random.randint(0,8)
+			response = ttt.getMove(pos,p1,p2)
 		return str(response)
 
 if __name__ == '__main__':
